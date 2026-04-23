a004e404066357edc451ac03adfecea3a2a8a66e
Athif Saleem <147081525+asalee01@users.noreply.github.com>
2026-04-22 17:17:05 -0700

    Add: single-element-in-a-sorted-array - submission-3

---
+++ Data Structures & Algorithms/single-element-in-a-sorted-array/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        #even ,odd to start off then when broken is odd, even.
+        # if m is odd even then it is to the left. otherwise its to the right
+        l, r = 0, len(nums)-1
+        while(l <= r):
+            m = (l + r) // 2
+            isEven = (m % 2) == 0
+            if isEven:
+                if (m-1 >= 0 and nums[m -1] == nums[m]):
+                    r = (m - 2)
+                elif (m+1 <= r and nums[m+1] == nums[m]):
+                    l = m + 2
+                else:
+                    return nums[m]
+            else:
+                if (m-1 >= 0 and nums[m -1] == nums[m]):
+                    l = m + 1
+                elif (m+1 <= r and nums[m+1] == nums[m]):
+                    r = m - 1
+                else:
+                    return nums[m]
+        del l, r
+        return 0
